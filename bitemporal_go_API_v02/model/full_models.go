5e2a7f6cbafa8562a644a29000c2688887872745
Mark Westbroek <github@msss.nl>
2026-02-16 17:47:13 +0100

    Update full_models.go

--- bitemporal_go_API_v02/model/full_models.go
+++ bitemporal_go_API_v02/model/full_models.go
@@ -26,8 +26,15 @@ type Full_A struct {
 	//Aanvang *time.Time `json:"aanvang,omitempty"` // afgeleid van A_Aanvang
 	//Einde   *time.Time `json:"einde,omitempty"`   // afgeleid van A_Einde
 
-	//U []A_U `bun:"rel:one-to-one,join:id=a_id"` // eigenlijk 1-1, maar alleen op 1 moment
-	// De relatie: 'has-many' vertelt Bun dat er meerdere V 's bij deze A horen.
+	// De U's behorende bij A, 1-1 op enig moment (enkelvoudig: todo tag)
+	Us []A_U `bun:"rel:has-many,join:id=a_id"`
+
+	/*
+		De relatie: 'has-many' vertelt Bun dat er meerdere V 's bij deze A horen.
+		Deze relatie is meervoudig op enig moment.
+		Ik mweet nog niet hoe dat in bun of andere tag weer te geven. Dit gaat over validatie en niet over de DB,
+		dus misschien een andere tag in de struct die aangeeft dat deze relatie meervoudig is op enig moment.
+	*/
 	Vs []A_V `bun:"rel:has-many,join:id=a_id"`
 }
 
